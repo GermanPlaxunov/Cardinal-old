b79816924f4aae15dcf5ef2957d5ce90d0476b28
German <snipe9029quant@gmail.com>
2023-07-07 23:40:06 +0300

    ver 0.0.5 --> Model added.

--- core/src/main/java/org/project/core/config/CoreBeansConfig.java
+++ core/src/main/java/org/project/core/config/CoreBeansConfig.java
@@ -1,7 +1,15 @@
 package org.project.core.config;
 
+import org.project.core.core.process.ProcessStarter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CoreBeansConfig {
+
+    @Bean
+    public ProcessStarter processStarter() {
+        return new ProcessStarter();
+    }
+
 }
---
+++ model/src/main/java/org/project/Task.java
@@ -0,0 +1,10 @@
+package org.project;
+
+import lombok.Data;
+
+@Data
+public class Task {
+
+    private String stock;
+
+}
