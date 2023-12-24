462c5031a4d502e0f205b605a9a6f08bfc1ec2d1
German <snipe9029quant@gmail.com>
2023-12-24 21:24:46 +0300

    feature/L-67 --> gate renamed to heimdall.

--- gate/src/main/java/org/project/gate/client/NeuralClient.java
+++
@@ -1,7 +0,0 @@
-package org.project.gate.client;
-
-public interface NeuralClient {
-
-    void train(String symbol);
-
-}
---
+++ heimdall/src/main/java/org/project/gate/job/ScheduledJobExecutor.java
@@ -0,0 +1,15 @@
+package org.project.gate.job;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ScheduledJobExecutor {
+
+
+    public void trade() {
+    }
+
+    public void train() {
+    }
+
+}
