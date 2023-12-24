632df75d4423eff9aa3d4003380009bb12f26dc5
German <snipe9029quant@gmail.com>
2023-12-24 21:04:04 +0300

    feature/L-68 --> cardinal launched.

---
+++ cardinal/src/main/java/org/libra/cardinal/rest/CardinalRestController.java
@@ -0,0 +1,12 @@
+package org.libra.cardinal.rest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CardinalRestController implements CardinalController {
+
+}
